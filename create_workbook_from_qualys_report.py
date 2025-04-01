7302eae01ad71116bac48db7a726a172497844ae
jblion <jerome.blion@free.fr>
2025-04-01 17:03:28 +0200

    Update create_workbook_from_qualys_report.py
    Empty sections management
    errors are in a separate sheet.

--- create_workbook_from_qualys_report.py
+++ create_workbook_from_qualys_report.py
@@ -14,30 +14,32 @@ def split_file(input_filename, output_file):
     try:
         with open(input_filename, "r", encoding="utf-8") as file:
             current_section = None
-            skip_this_line=False
+            skip_this_line = False
 
-            # Expressions régulières pour détecter les sections avec correspondance exacte
+            # Regular expressions to detect sections with exact match
             patterns = {
                 "summary": re.compile(r"^Host Statistics \(Percentage of Controls Passed per Host\)$"),
                 "assets": re.compile(r"^ASSET TAGS$"),
-                "results": re.compile(r"^RESULTS$")
+                "results": re.compile(r"^RESULTS$"),
+                "errors": re.compile(r"^Possible reason for empty report$")
             }
 
             wb = Workbook(write_only=True)
 
             for row in csv.reader(file):
-                for section, regex in patterns.items():
-                    if regex.match(row[0]):
-                        current_section = section
-                        current_ws=wb.create_sheet(current_section)
-                        skip_this_line=True
-                        print ("Processing "+ current_section)
-                        break  # On sort de la boucle dès qu'on trouve une correspondance
-                    else:
-                        skip_this_line=False
-                        
-                if(current_section and skip_this_line == False):
-                    current_ws.append(row)
+                if row:
+                    for section, regex in patterns.items():
+                        if regex.match(row[0]):
+                            current_section = section
+                            current_ws = wb.create_sheet(current_section)
+                            skip_this_line = True
+                            print ("Processing "+ current_section)
+                            break  # Let's get out of the loop
+                        else:
+                            skip_this_line=False
+                                      
+                    if(current_section and skip_this_line == False):
+                        current_ws.append(row)
         
         print("Saving output file: " + output_file)
         wb.save(output_file)
@@ -46,10 +48,12 @@ def split_file(input_filename, output_file):
         print(f"Error: The file '{input_filename}' was not found.")
     except PermissionError:
         print(f"Error: Permission denied when trying to read '{input_filename}'.")
+    except IndexError:
+        print(len(row))
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
 
-# Vérification et exécution
+# Check before run
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         print("Usage : python " + sys.argv[0] + " input_file.csv output_file")
